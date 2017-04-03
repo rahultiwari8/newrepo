4931f099a0fdef7d61a0ee3e3efb986223726993
Rahul Tiwari <rahultiwari@Rahuls-Mac-mini.local>
2017-04-03 23:07:12 +0530

    initial commit

---
+++ src/testcases/MyfirstTest.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.junit.Test;
+
+public class MyfirstTest {
+	
+	@Test //Test Case
+	public void xyztest()
+	{
+		System.out.println("Test in progress");
+	}
+	
+	@Test
+	public void register()
+	{
+		System.out.println("registering");
+	}
+	
+
+}
